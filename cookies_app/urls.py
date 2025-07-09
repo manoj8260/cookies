e6e1f0880717309098925c6e105d9fe077f678d1
manoj8260 <mn047653@gmail.com>
2025-07-09 14:21:58 +0530

    updated code

--- cookies_app/urls.py
+++ cookies_app/urls.py
@@ -4,5 +4,7 @@ urlpatterns =[
     path('set/',set_cookies,) ,
     path('get/',get_cookies,) ,
     path('del/',del_cookies,) , 
+    path('setsigned/',setsigned_cookies,) , 
+    path('getsigned/',getsined_cookies,) , 
    
 ]
--- cookies_app/views.py
+++ cookies_app/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-
+from datetime import datetime ,timezone ,timedelta
 # Create your views here.
 
 def set_cookies(request):
     response = render(request,'setcookies.html')
+    response.set_cookie('token','token7732218',max_age=3600)
+    response.set_cookie('payid','payid111111',expires=datetime.now(timezone.utc)+(timedelta(days=2)))
     return response
 def get_cookies(request):
-    response = render(request,'getcookies.html')
+    cookies = request.COOKIES.get('token')
+    response = render(request,'getcookies.html' ,{'cookies' : cookies})
     return response
 def del_cookies(request):
     response = render(request,'delcookies.html')
+    response.delete_cookie('token')
     return response
+
+def setsigned_cookies(request):
+    response = render(request,'setsignedcookies.html')
+    response.set_signed_cookie('s_token' , 'stoken123577' ,salt='stk')
+    return  response
+def getsined_cookies(request):
+    token = request.get_signed_cookie('s_token',salt = 'stk',default ='defaulttoken-1345')
+    response = render(request,'getsinedcookies.html' , {'token' :token})
+    return  response
